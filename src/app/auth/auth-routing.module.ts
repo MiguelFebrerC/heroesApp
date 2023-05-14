5d05bf46787ff2c97ef87de2340d115f73dddc0d
mfebrerc <miguel.febrer-capo@capgemini.com>
2023-05-14 21:17:32 +0200

    added layout login, new user and menu

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
   {
@@ -9,7 +10,7 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: 'login', component: LoginPageComponent },
-      { path: 'new-account', component: LoginPageComponent },
+      { path: 'new-account', component: RegisterPageComponent },
       { path: '**', redirectTo: 'login' },
     ],
   },
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,17 +5,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
-
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [
-    LayoutComponent,
-    LoginPageComponent,
-    RegisterPageComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LayoutComponent, LoginPageComponent, RegisterPageComponent],
+  imports: [CommonModule, AuthRoutingModule, MaterialModule],
 })
-export class AuthModule { }
+export class AuthModule {}
